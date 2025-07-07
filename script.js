4362303261f5984baef081662c749c3552c8f27b
kolbybo <kbonds1469@gmail.com>
2025-07-07 00:30:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+//get html elements
+const btn = document.getElementById("btn");
+const restartbtn = document.querySelector(".redobtn");
+const formScreen = document.querySelector(".container");
+const resultScreen = document.querySelector(".resultScreen");
+const jobNameText = document.getElementById("name");
+const scope = document.getElementById("scope");
+const facilityName = document.getElementById("facilityName");
+const scopeResultText = document.getElementById("facilityScope");
+const facilityResultText = document.getElementById("facilityResult");
+const greeting = document.querySelector('.greeting')
+const userName = document.getElementById("userName");
 
+btn.addEventListener("click", createJobName);
+function createJobName() {
+  facilityName.value;
+  scope.value;
+  userName.style.color = 'red';
+  formScreen.classList.add("hidden");
+  resultScreen.classList.remove("hidden");
+  jobNameText.textContent = `${facilityName.value}${scope.value}`;
+  scopeResultText.textContent = scope.options[scope.selectedIndex].text;
+  facilityResultText.textContent = facilityName.value
+  greeting.textContent = userName.value
+}
 
-
-
-//create basic job names
-const btn = document.getElementById('btn')
-
-btn.addEventListener('click', createJobName) 
-function createJobName (){
-const facilityName = document.getElementById('facilityName').value;
-const select = document.getElementById('scope').value
-
-document.getElementById('name').textContent = `${facilityName}${select}`
-} 
+restartbtn.addEventListener("click", redoForm);
+function redoForm() {
+  document.getElementById("jobNames").reset();
+  document.querySelector(".resultScreen").classList.add("hidden");
+  document.querySelector(".container").classList.remove("hidden");
+}
